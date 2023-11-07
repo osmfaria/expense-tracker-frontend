8e4bccf83bb4f343544571651ac82f96b2683f58
Osmar Faria <osm.faria@gmail.com>
2023-11-06 18:51:02 -0800

    feat: implemented toastify setting and add container

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,8 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import { Flip, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +17,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
+        <ToastContainer
+          position='top-right'
+          autoClose={3000}
+          limit={1}
+          hideProgressBar={true}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss={false}
+          draggable
+          pauseOnHover={false}
+          theme='light'
+          transition={Flip}
+        />
         <Stack
           direction='column'
           justifyContent='space-between'
