684a77c6791a98f3aee714591bd5af5ac7f91af9
Osmar Faria <osm.faria@gmail.com>
2023-11-07 10:24:11 -0800

    refactor: adjust input css

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Navbar from '@/components/Navbar'
+import { Container } from '@mui/material'
+import ExpenseForm from '@/components/ExpenseForm'
 
 export default function Home() {
-  return <>Hi</>
+  return (
+    <Container maxWidth='lg'>
+      <ExpenseForm />
+    </Container>
+  )
 }
