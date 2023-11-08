504f8fa77d000c99eaa589147af50ab24690b49c
Osmar Faria <82468282+osmfaria@users.noreply.github.com>
2023-11-07 21:47:41 -0800

    feat: implement error handling and activeEmployee state

--- src/providers/employee.js
+++ src/providers/employee.js
@@ -8,18 +8,34 @@ const EmployeeContext = createContext()
 
 export const EmployeeProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false)
-  const [employees, setEmployees] = useState()
+  const [employees, setEmployees] = useState([])
+  const [activeEmployee, setActiveEmployee] = useState('')
+  const [error, setError] = useState(false)
+
+  const selectActiveEmployee = (employee) => setActiveEmployee(employee)
 
   const getEmployees = async () => {
     setIsLoading(true)
     await Api.get('/users')
       .then((res) => setEmployees(res.data))
-      .catch((_) => toast.error('Something went wrong... Try again later'))
+      .catch((_) => {
+        setError(true)
+        toast.error('Something went wrong... Try again later')
+      })
       .finally(() => setIsLoading(false))
   }
 
   return (
-    <EmployeeContext.Provider value={{ employees, getEmployees, isLoading }}>
+    <EmployeeContext.Provider
+      value={{
+        employees,
+        getEmployees,
+        isLoading,
+        selectActiveEmployee,
+        activeEmployee,
+        error,
+      }}
+    >
       {children}
     </EmployeeContext.Provider>
   )
