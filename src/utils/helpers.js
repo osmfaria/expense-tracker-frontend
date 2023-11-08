3f0331f25de182986cd6168a6ea6200a4c60e06a
Osmar Faria <82468282+osmfaria@users.noreply.github.com>
2023-11-08 02:57:52 -0800

    feat: implement function to format date, currency andavatar

---
+++ src/utils/helpers.js
@@ -0,0 +1,15 @@
+import dayjs from 'dayjs'
+
+export const formateDate = (date) => dayjs(date).format('YYYY/MM/DD')
+
+export const formateCurrency = (value) =>
+  Number(value).toLocaleString('en-Us', {
+    style: 'currency',
+    currency: 'USD',
+  })
+
+export const stringAvatar = (name) => {
+  return {
+    children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+  }
+}
