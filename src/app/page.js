7e6354435e27eeaaae21b16978f126e18e13da81
Osmar Faria <82468282+osmfaria@users.noreply.github.com>
2023-11-08 03:01:33 -0800

    feat: add components to improve responsiviness

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,24 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Navbar from '@/components/Navbar'
-import { Container } from '@mui/material'
+import { Box, Container, Stack } from '@mui/material'
 import ExpenseForm from '@/components/ExpenseForm'
 import Employee from '@/components/Employee'
+import ExpenseDisplay from '@/components/ExpenseDisplay'
 
 export default function Home() {
   return (
     <Container maxWidth='lg'>
-      <Employee />
-      <ExpenseForm />
+      <Stack
+        gap={2}
+        sx={{
+          flexDirection: { xs: 'column', lg: 'row' },
+          alignItems: { xs: 'center', lg: 'flex-start' },
+        }}
+      >
+        <Box sx={{ flexGrow: 1, width: { xs: 'auto', lg: '70%' } }}>
+          <Employee />
+          <ExpenseForm />
+        </Box>
+        <ExpenseDisplay />
+      </Stack>
     </Container>
   )
 }
