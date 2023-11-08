2663f76c90cd59d299407bbbdde046f2f9512de8
Osmar Faria <82468282+osmfaria@users.noreply.github.com>
2023-11-07 21:45:26 -0800

    feat: add employee component

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,12 @@ import styles from './page.module.css'
 import Navbar from '@/components/Navbar'
 import { Container } from '@mui/material'
 import ExpenseForm from '@/components/ExpenseForm'
+import Employee from '@/components/Employee'
 
 export default function Home() {
   return (
     <Container maxWidth='lg'>
+      <Employee />
       <ExpenseForm />
     </Container>
   )
