c281d5b2dd05d29cb7d52e95e7a842fcd688bb68
Osmar Faria <82468282+osmfaria@users.noreply.github.com>
2023-11-09 07:23:00 -0800

    feat: connect table with api data

--- src/components/ExpenseTable.js
+++ src/components/ExpenseTable.js
@@ -11,35 +11,18 @@ import {
 } from '@mui/material'
 import React from 'react'
 import { formateCurrency, formatDate } from '@/utils/helpers'
+import { useExpense } from '@/providers/expenses'
 
 const ExpenseTable = () => {
-  const data = [
-    {
-      id: '8428155b-f1dd-463f-b5fd-cad6c1e4ed47',
-      date: '2023-10-08T00:00:00.000Z',
-      description: 'dinner with clients',
-      amount: 22.25,
-      userId: '7858415e-453d-4196-9072-28e23d98029b',
-    },
-    {
-      id: 'ee61b740-fc54-4052-83be-eb30c9b3c001',
-      date: '2023-08-08T00:00:00.000Z',
-      description: 'Taxi to airport',
-      amount: 17.2,
-      userId: '7858415e-453d-4196-9072-28e23d98029b',
-    },
-    {
-      id: 'e3292d95-cf54-461d-9b82-eaa7a0db31ff',
-      date: '2023-04-14T00:00:00.000Z',
-      description: 'Flight to Toronto',
-      amount: 177.2,
-      userId: '7858415e-453d-4196-9072-28e23d98029b',
-    },
-  ]
+  const { expenses } = useExpense()
 
   return (
     <TableContainer
-      sx={{ overflow: 'auto', maxHeight: { xs: '300px', md: '500px' } }}
+      sx={{
+        overflow: 'auto',
+        height: '300px',
+        paddingBottom: '20px',
+      }}
     >
       <Table>
         <TableHead>
@@ -62,7 +45,7 @@ const ExpenseTable = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {data.map((expense) => (
+          {expenses.map((expense) => (
             <TableRow key={expense.id}>
               <TableCell sx={{ whiteSpace: 'nowrap' }}>
                 {formatDate(expense.date)}
