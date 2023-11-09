a0c83aae72602ea1758cc35a8be2111f9a38e273
Osmar Faria <82468282+osmfaria@users.noreply.github.com>
2023-11-09 07:13:37 -0800

    feat: add material ui theme

---
+++ src/components/Theme.js
@@ -0,0 +1,14 @@
+'use client'
+
+import { createTheme } from '@mui/material/styles'
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1B1B1B',
+    },
+    secondary: {
+      main: '#8c0034',
+    },
+  },
+})
