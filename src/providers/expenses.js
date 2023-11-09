512ec723238764fe6651daaa92445ce278b1f1b5
Osmar Faria <82468282+osmfaria@users.noreply.github.com>
2023-11-09 07:28:23 -0800

    feat: implement provider to control weekly view
    refactor: return response from api on create expense
    
    feat: add onExpenseCreation to rerender expenses when adding new ones

--- src/providers/expenses.js
+++ src/providers/expenses.js
@@ -7,18 +7,28 @@ import { toast } from 'react-toastify'
 const ExpenseContext = createContext()
 
 export const ExpenseProvider = ({ children }) => {
-  const [expenses, setExpenses] = useState()
-  const [expensesByWeek, setExpensesByWeek] = useState()
-  const [expenseYears, setExpensesYears] = useState()
+  const [expenses, setExpenses] = useState([])
+  const [expensesByWeek, setExpensesByWeek] = useState([])
+  const [expenseYears, setExpensesYears] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const [view, setView] = useState('monthly')
+
+  const handleView = () => {
+    setView((prev) => (prev === 'monthly' ? 'weekly' : 'monthly'))
+  }
 
   const createExpense = async (data) => {
     setIsLoading(true)
 
-    await Api.post(`/expenses/`, data)
-      .then((_) => toast.success('Expense recorded'))
+    const response = await Api.post(`/expenses/`, data)
+      .then((res) => {
+        toast.success('Expense recorded')
+        return res
+      })
       .catch((_) => toast.error('Something went wrong... Try again later'))
       .finally(() => setIsLoading(false))
+
+    return response
   }
 
   const getExpenses = async (userId, year) => {
@@ -61,6 +71,12 @@ export const ExpenseProvider = ({ children }) => {
       .finally(() => setIsLoading(false))
   }
 
+  const onExpenseCreation = async (userId, year) => {
+    getYearsWithExpenses(userId)
+    getExpenses(userId, year)
+    getExpensesByWeek(userId, year)
+  }
+
   return (
     <ExpenseContext.Provider
       value={{
@@ -74,6 +90,9 @@ export const ExpenseProvider = ({ children }) => {
         createExpense,
         updateExpense,
         isLoading,
+        view,
+        handleView,
+        onExpenseCreation,
       }}
     >
       {children}
