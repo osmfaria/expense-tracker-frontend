e59fb8823bfcbaa90dedc59fbcdf3b5d62edf591
Osmar Faria <82468282+osmfaria@users.noreply.github.com>
2023-11-09 07:04:54 -0800

    feat: implement filter dashboard to fetch expenses, filter by year and group by week

---
+++ src/components/ExpenseFilters.js
@@ -0,0 +1,106 @@
+'use client'
+
+import {
+  Box,
+  Card,
+  LinearProgress,
+  Stack,
+  Switch,
+  Typography,
+} from '@mui/material'
+import YearsDropDown from './YearsDropDown'
+import { useExpense } from '@/providers/expenses'
+import { useEffect, useState } from 'react'
+import { useEmployee } from '@/providers/employee'
+
+const ExpenseFilters = () => {
+  const [activeYear, setActiveYear] = useState('')
+  const {
+    getYearsWithExpenses,
+    getExpensesByWeek,
+    getExpenses,
+    expenseYears,
+    view,
+    handleView,
+    isLoading,
+  } = useExpense()
+  const { activeEmployee } = useEmployee()
+
+  const handleChangeYears = (event) => {
+    setActiveYear(event.target.value)
+  }
+
+  useEffect(() => {
+    if (activeEmployee) {
+      getYearsWithExpenses(activeEmployee)
+    }
+  }, [activeEmployee])
+
+  useEffect(() => {
+    if (expenseYears && expenseYears.length > 0) {
+      setActiveYear(expenseYears[0].year)
+    }
+  }, [expenseYears])
+
+  useEffect(() => {
+    if (activeEmployee && activeYear) {
+      getExpenses(activeEmployee, activeYear)
+      getExpensesByWeek(activeEmployee, activeYear)
+    }
+  }, [activeEmployee, activeYear])
+
+  return (
+    <Card
+      sx={{
+        width: '100%',
+        mb: '20px',
+        overflowY: 'hidden',
+      }}
+    >
+      <Box style={{ height: '4px', overflowY: 'hidden' }}>
+        {isLoading && <LinearProgress />}
+      </Box>
+      <Box
+        sx={{
+          padding: { xs: '20px 15px', md: '25px 30px' },
+        }}
+      >
+        <Typography
+          variant='h5'
+          color='primary'
+          mb='20px'
+          sx={{ fontSize: { xs: '1rem', sm: '1.2rem' } }}
+        >
+          Filters
+        </Typography>
+        <Stack direction='row' alignItems='flex-start' gap={4}>
+          <YearsDropDown
+            label={'Years'}
+            value={activeYear}
+            handleChange={handleChangeYears}
+            disabled={!expenseYears.length || isLoading}
+            list={expenseYears}
+          />
+          <Stack direction='row' alignItems='center' gap={'2px'} width='50%'>
+            <Switch
+              checked={view === 'weekly'}
+              onChange={handleView}
+              disabled={!activeEmployee || isLoading}
+              color='secondary'
+            />
+            <Typography
+              variant='body2'
+              fontWeight='500'
+              color='MenuText'
+              fontSize='1rem'
+            >
+              Group by Week
+            </Typography>
+          </Stack>
+        </Stack>
+      </Box>
+    </Card>
+  )
+}
+
+export default ExpenseFilters
