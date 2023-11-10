218bad5cffdf22f883bb48e39cfc3f72763bc952
Osmar Faria <82468282+osmfaria@users.noreply.github.com>
2023-11-09 21:04:55 -0800

    feat: add edit modal and control functions
    add table edit button

--- src/components/ExpenseTable.js
+++ src/components/ExpenseTable.js
@@ -1,23 +1,46 @@
 'use client'
 
 import {
+  IconButton,
+  Stack,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableHead,
   TableRow,
+  Tooltip,
   Typography,
 } from '@mui/material'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { formateCurrency, formatDate } from '@/utils/helpers'
 import { useExpense } from '@/providers/expenses'
 import { useEmployee } from '@/providers/employee'
 import DeleteButton from './DeleteButton'
+import { EditNote } from '@mui/icons-material'
+import UpdateExpenseModal from './UpdateExpenseModal'
 
 const ExpenseTable = () => {
   const { expenses, deleteExpense, onExpenseDelete, isLoading } = useExpense()
   const { activeEmployee } = useEmployee()
+  const [isOpen, setIsOpen] = useState(false)
+  const [expenseToEdit, setExpenseToEdit] = useState(null)
+  const tableContainerRef = useRef(null)
+
+  useEffect(() => {
+    if (tableContainerRef.current) {
+      tableContainerRef.current.scrollTop = 0
+    }
+  }, [expenses])
+
+  const handleModal = (expense) => {
+    if (isOpen) {
+      setExpenseToEdit(null)
+    } else {
+      setExpenseToEdit(expense)
+    }
+    setIsOpen((prev) => !prev)
+  }
 
   const handleDelete = async (expenseId, expenseDate) => {
     const res = await deleteExpense(expenseId)
@@ -28,57 +51,77 @@ const ExpenseTable = () => {
   }
 
   return (
-    <TableContainer
-      sx={{
-        overflow: 'auto',
-        height: '300px',
-        paddingBottom: '20px',
-      }}
-    >
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              <Typography variant='inherit' fontWeight={600}>
-                Date
-              </Typography>{' '}
-            </TableCell>
-            <TableCell>
-              <Typography variant='inherit' fontWeight={600}>
-                Description
-              </Typography>{' '}
-            </TableCell>
-            <TableCell>
-              <Typography variant='inherit' fontWeight={600}>
-                Amount
-              </Typography>{' '}
-            </TableCell>
-            <TableCell></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {expenses.map((expense) => (
-            <TableRow key={expense.id}>
-              <TableCell sx={{ whiteSpace: 'nowrap' }}>
-                {formatDate(expense.date)}
-              </TableCell>
-              <TableCell sx={{ whiteSpace: 'nowrap' }}>
-                {expense.description}
-              </TableCell>
-              <TableCell sx={{ whiteSpace: 'nowrap' }}>
-                {formateCurrency(expense.amount)}
+    <>
+      {expenseToEdit && (
+        <UpdateExpenseModal
+          handleClose={handleModal}
+          isOpen={isOpen}
+          expenseToEdit={expenseToEdit}
+        />
+      )}
+      <TableContainer
+        sx={{
+          overflow: 'auto',
+          height: '300px',
+          paddingBottom: '20px',
+          scrollBehavior: 'smooth',
+        }}
+        ref={tableContainerRef}
+      >
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <Typography variant='inherit' fontWeight={600}>
+                  Date
+                </Typography>{' '}
               </TableCell>
               <TableCell>
-                <DeleteButton
-                  handleDelete={() => handleDelete(expense.id, expense.date)}
-                  isLoading={isLoading}
-                />
+                <Typography variant='inherit' fontWeight={600}>
+                  Description
+                </Typography>{' '}
               </TableCell>
+              <TableCell>
+                <Typography variant='inherit' fontWeight={600}>
+                  Amount
+                </Typography>{' '}
+              </TableCell>
+              <TableCell></TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {expenses.map((expense) => (
+              <TableRow key={expense.id}>
+                <TableCell sx={{ whiteSpace: 'nowrap' }}>
+                  {formatDate(expense.date)}
+                </TableCell>
+                <TableCell sx={{ whiteSpace: 'nowrap' }}>
+                  {expense.description}
+                </TableCell>
+                <TableCell sx={{ whiteSpace: 'nowrap' }}>
+                  {formateCurrency(expense.amount)}
+                </TableCell>
+                <TableCell>
+                  <Stack direction='row' flexWrap='nowrap' alignItems='center'>
+                    <DeleteButton
+                      handleDelete={() =>
+                        handleDelete(expense.id, expense.date)
+                      }
+                      isLoading={isLoading}
+                    />
+                    <Tooltip title='Edit'>
+                      <IconButton onClick={() => handleModal(expense)}>
+                        <EditNote />
+                      </IconButton>
+                    </Tooltip>
+                  </Stack>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   )
 }
 
